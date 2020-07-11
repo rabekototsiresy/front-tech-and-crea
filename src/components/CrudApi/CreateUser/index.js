9d5748a99b295bd6ee9676bab751cc5d15f3f75b
Tsiresy <tsiresyrabekoto@gmail.com>
2020-07-11 22:47:14 +0200

    fix: remove path component BtnSubmit

--- src/components/CrudApi/CreateUser/index.js
+++ src/components/CrudApi/CreateUser/index.js
@@ -1,7 +1,6 @@
 import React,{useState} from 'react'
 import { createUser } from '../../../redux'
 import { useDispatch,useSelector } from 'react-redux'
-import ButtonSubmit from '../../SousComponents/ButtonSubmit';
 import Message from '../../Message/Main';
 import Button from '../../SousComponents/Button';
 
--- src/components/CrudApi/UpdateUser/index.js
+++ src/components/CrudApi/UpdateUser/index.js
@@ -1,7 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import { useDispatch,useSelector } from 'react-redux'
 import { makeUserUpdate } from '../../../redux'
-import ButtonSubmit from '../../SousComponents/ButtonSubmit';
 import Message from '../../Message/Main';
 import Button from '../../SousComponents/Button';
 
